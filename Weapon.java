514b07adedf22baf309cd2cc4013fc9fe95b58d4
MelikeeKorkmaz <melikekorkmaz099@gmail.com>
2024-10-08 12:17:26 +0300

    Create Weapon.java

---
+++ Weapon.java
@@ -0,0 +1,45 @@
+public class Weapon {
+    private int id;
+    private String name;
+    private int damage;
+    private int cost;
+
+    Weapon(int id, String name, int damage, int cost){
+        this.id = id;
+        this.name = name;
+        this.damage = damage;
+        this.cost = cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
