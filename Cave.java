7de4360fb35587477e7c52132237929a88c37f50
MelikeeKorkmaz <melikekorkmaz099@gmail.com>
2024-10-13 18:23:01 +0300

    Update Cave.java

--- Cave.java
+++ Cave.java
@@ -1,15 +1,27 @@
 public class Cave extends BattleLoc{
     Cave(Player player){
-        super("Mağara");
+        super();
+        setLocName("Mağara");
         setPlayer(player);
+        setObstacle(new Zombie());
     }
 
     @Override
     public boolean onLocation(){
-        System.out.println("Mağaraya Hoş geldiniz!");
-        do {
-            setObsCount();
-        }while(getObsCount() == 0);
+        System.out.println("---Mağaraya Hoş Geldin---");
+        System.out.println("Mağarayı zombilerden temizleyebilirsen, karşılığında bir miktar para ve yemek alacaksın. Yoksa açlıktan ölmeyi mi tercih edersin?");
+        start();
+        if (getPlayer().getHealth() > 0 && getCount() == 0) {
+            System.out.println("Tebrikler! " + getObstacle().getName() + " yok edildi. Bölge temizlendi");
+            getPlayer().getInventory().setFood(true);
+            System.out.println("Envanterine yemek eklendi");
+            if(isWin()){
+                return false;
+            }
+        }else if(getPlayer().getHealth() <= 0){
+            System.out.println("Öldünüz!\n----GAME OVER----");
+            return false;
+        }
         return true;
     }
 }
