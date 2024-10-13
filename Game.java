a990cff7448e694b46c87ff92949850125380b79
MelikeeKorkmaz <melikekorkmaz099@gmail.com>
2024-10-13 17:12:34 +0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -6,17 +6,19 @@ public class Game {
     public void start() {
         System.out.println("Hoş geldin!");
         System.out.println("Karşına çıkan engellerle, hayatta kalmak uğruna savaşacaksın. Yemek, su ve odun bulduğunda amacına ulaşmış olursun. Dikkatli ol! Kolay olmayacak...");
+        System.out.println("Bölgeleri canavarlardan temizleyebilirsen eşyalar senin olur. Her öldürdüğün canavar için bir miktar para kazanacaksın.");
         System.out.println("    ...    ");
         System.out.print("Karakter adınızı girin: ");
         String name = input.nextLine();
         Player n1 = new Player(name);
 
+        GameChar[] chars = {new Samurai(), new Archer(), new Knight()};
         System.out.println("Sıra karakterde... Nasıl bir karakter oynamak isterdin? ");
-        n1.selectChar();
+        n1.selectChar(chars);
 
-        Location[] locations = {new SafeHouse(n1), new ToolStore(n1),  new Forest(n1), new River(n1), new Cave(n1)};
+        Location[] locations = {new SafeHouse(n1), new ToolStore(n1),  new Cave(n1), new Forest(n1), new River(n1)};
         n1.setLoc(locations[0]);
         n1.selectLoc(locations);
-    }
 
+    }
 }
