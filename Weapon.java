c9c7d2d6e2bd7965095cc48bf74e2664d8fa5969
MelikeeKorkmaz <melikekorkmaz099@gmail.com>
2024-10-13 18:30:21 +0300

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -15,16 +15,16 @@ public class Weapon {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public int getDamage() {
