8567923982635ffc3cee92822addabba8e6d34c2
MelikeeKorkmaz <melikekorkmaz099@gmail.com>
2024-10-13 18:24:44 +0300

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,10 +1,16 @@
 public class Inventory {
     Weapon weapon;
     Armor armor;
+    private boolean food;
+    private boolean water;
+    private boolean firewood;
 
     Inventory() {
         this.weapon = new Weapon(0, "Yumruk", 0, 0);
         this.armor = new Armor(0, "Kıyafet", 0, 0);
+        this.food = false;
+        this.water = false;
+        this.firewood = false;
     }
 
     public Weapon getWeapon() {
@@ -22,5 +28,28 @@ public class Inventory {
     public void setArmor(Armor armor) {
         this.armor = armor;
     }
-}
 
+    public boolean isFood() {
+        return food;
+    }
+
+    public void setFood(boolean food) {
+        this.food = food;
+    }
+
+    public boolean isWater() {
+        return water;
+    }
+
+    public void setWater(boolean water) {
+        this.water = water;
+    }
+
+    public boolean isFirewood() {
+        return firewood;
+    }
+
+    public void setFirewood(boolean firewood) {
+        this.firewood = firewood;
+    }
+}
