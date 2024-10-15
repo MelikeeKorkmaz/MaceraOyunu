dba20c2ffb1f051fec2301f6c8853547e3867796
MelikeeKorkmaz <melikekorkmaz099@gmail.com>
2024-10-15 21:05:26 +0300

    Update NormalLoc.java

--- NormalLoc.java
+++ NormalLoc.java
@@ -2,10 +2,4 @@ public abstract class NormalLoc extends Location{
     public NormalLoc(){
         super();
     }
-
-    @Override
-    public boolean onLocation(){
-        System.out.println("Güvenli Bölgeler");
-        return true;
-    }
 }
