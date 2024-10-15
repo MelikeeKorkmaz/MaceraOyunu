f595d8655754a88116393fb12c0391e4e63f468a
MelikeeKorkmaz <melikekorkmaz099@gmail.com>
2024-10-15 22:26:11 +0300

    Update SafeHouse.java

--- SafeHouse.java
+++ SafeHouse.java
@@ -1,12 +1,12 @@
-public class SafeHouse extends NormalLoc{
-    public SafeHouse(Player player){
+public class SafeHouse extends Location {
+    public SafeHouse(Player player) {
         super();
         setLocName("Güvenli Ev");
         setPlayer(player);
     }
 
     @Override
-    public boolean onLocation(){
+    public boolean onLocation() {
         System.out.println("Güvenli evdesiniz. Sağlığınız yenilenecektir. Keyfinize bakın burada kimse size zarar veremez.");
         getPlayer().setHealth(getPlayer().getGameChar().getHealth());
         System.out.println("Sağlık kontrolünüz yapıldı. Daha iyi gözüküyorsunuz ");
