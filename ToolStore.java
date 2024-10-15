00d3c73c4929a3c9b2c69808ae3522dfe43416a6
MelikeeKorkmaz <melikekorkmaz099@gmail.com>
2024-10-15 22:26:40 +0300

    Update ToolStore.java

--- ToolStore.java
+++ ToolStore.java
@@ -1,6 +1,6 @@
-public class ToolStore extends NormalLoc {
-    Weapon[] weapons = {new Weapon(1,"Tabanca",2,25), new Weapon(2,"Kılıç",3,35),new Weapon(3,"Tüfek",7,45)};
-    Armor[] armors = {new Armor(1, "Light",1,15),new Armor(2,"Medium",3,25),new Armor(3,"Heavy",5,40)};
+public class ToolStore extends Location {
+    Weapon[] weapons = {new Weapon(1, "Tabanca", 2, 25), new Weapon(2, "Kılıç", 3, 35), new Weapon(3, "Tüfek", 7, 45)};
+    Armor[] armors = {new Armor(1, "Light", 1, 15), new Armor(2, "Medium", 3, 25), new Armor(3, "Heavy", 5, 40)};
 
     public ToolStore(Player player) {
         super();
@@ -26,21 +26,21 @@ public class ToolStore extends NormalLoc {
         return true;
     }
 
-    public void buyTool(int buy){
+    public void buyTool(int buy) {
         switch (buy) {
             case 1:
                 printWeapons(weapons);
                 System.out.print("Çıkış için herhangi bir sayıyı girebilirsiniz.\n Yapmak istediğiniz işlemin numarasını girin: ");
                 int c1 = input.nextInt();
                 System.out.println("========================");
-                if(c1 <= weapons.length && c1 > 0) {
-                    if ((getPlayer().getMoney()-weapons[c1-1].getCost()) < 0) {
-                        System.out.println(weapons[c1-1].getName() + " almak için yeterli paranız bulunmamaktadır!");
+                if (c1 <= weapons.length && c1 > 0) {
+                    if ((getPlayer().getMoney() - weapons[c1 - 1].getCost()) < 0) {
+                        System.out.println(weapons[c1 - 1].getName() + " almak için yeterli paranız bulunmamaktadır!");
                     } else {
-                        System.out.println(weapons[c1-1].getName() + " satın alma işlemi başarı ile gerçekleşmiştir.");
-                        getPlayer().setMoney(getPlayer().getMoney()-weapons[c1-1].getCost());
-                        getPlayer().getInventory().setWeapon(weapons[c1-1]);
-                        getPlayer().setDamage(getPlayer().getGameChar().getDamage() + weapons[c1-1].getDamage());
+                        System.out.println(weapons[c1 - 1].getName() + " satın alma işlemi başarı ile gerçekleşmiştir.");
+                        getPlayer().setMoney(getPlayer().getMoney() - weapons[c1 - 1].getCost());
+                        getPlayer().getInventory().setWeapon(weapons[c1 - 1]);
+                        getPlayer().setDamage(getPlayer().getGameChar().getDamage() + weapons[c1 - 1].getDamage());
                     }
                 }
                 break;
@@ -49,29 +49,29 @@ public class ToolStore extends NormalLoc {
                 System.out.print("Çıkış için herhangi bir sayıyı girebilirsiniz.\n Yapmak istediğiniz işlemin numarasını girin:");
                 int c2 = input.nextInt();
                 System.out.println("========================");
-                if(c2 <= armors.length && c2 > 0) {
-                    if ((getPlayer().getMoney()-armors[c2-1].getCost()) < 0) {
-                        System.out.println(armors[c2-1].getName() + " armoru almak için yeterli paranız bulunmamaktadır!");
+                if (c2 <= armors.length && c2 > 0) {
+                    if ((getPlayer().getMoney() - armors[c2 - 1].getCost()) < 0) {
+                        System.out.println(armors[c2 - 1].getName() + " armoru almak için yeterli paranız bulunmamaktadır!");
                     } else {
-                        System.out.println(armors[c2-1].getName() + " satın alma işlemi başarı ile gerçekleşmiştir.");
-                        getPlayer().setMoney(getPlayer().getMoney() - armors[c2-1].getCost());
-                        getPlayer().getInventory().setArmor(armors[c2-1]);
+                        System.out.println(armors[c2 - 1].getName() + " satın alma işlemi başarı ile gerçekleşmiştir.");
+                        getPlayer().setMoney(getPlayer().getMoney() - armors[c2 - 1].getCost());
+                        getPlayer().getInventory().setArmor(armors[c2 - 1]);
                     }
                 }
                 break;
         }
     }
 
-    public void printWeapons(Weapon[] weapons){
+    public void printWeapons(Weapon[] weapons) {
         System.out.println("-SİLAHLAR-");
-        for(Weapon i: weapons){
+        for (Weapon i : weapons) {
             System.out.println(i.getId() + "-" + i.getName() + " --->" + "\tHasar: " + i.getDamage() + "\tFiyat: " + i.getCost());
         }
     }
 
-    public void printArmors(Armor[] armors){
+    public void printArmors(Armor[] armors) {
         System.out.println("-ARMORLAR-");
-        for(Armor i: armors){
+        for (Armor i : armors) {
             System.out.println(i.getId() + "-" + i.getName() + " --->" + "\tKoruma: " + i.getProtection() + "\tFiyat: " + i.getCost());
         }
     }
